c942ed630afd3bfd6ad3a2293d1b60a58a0c6260
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-08-12 09:48:15 -0300

    fix: undetected clickout

--- src/useClickOut.tsx
+++ src/useClickOut.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo } from "react"
+import { useCallback, useEffect } from "react"
 
 interface useClickOutProps {
     onClickOut: () => void
@@ -6,88 +6,33 @@ interface useClickOutProps {
 }
 
 type Event = (TouchEvent | MouseEvent) & {
-    __clickedElements?: HTMLElement[]
+    __wrapperIsClicked?: boolean
 }
 
 export function useClickOut({ onClickOut, elementRef: wrapperRef }: useClickOutProps) {
-    const functions = useMemo(() => {
-        let elTouchIsClick = true
-        let documentTouchIsClick = true
-        const el = wrapperRef.current as HTMLDivElement
+    const wrapperClick = useCallback((ev: Event) => {
+        ev.__wrapperIsClicked = true
+    }, [])
 
-        const documentTouchMoved = () => { documentTouchIsClick = false }
-
-        const documentTouchStarted = () => {
-            el?.removeEventListener('click', elementClicked)
-            document.removeEventListener('click', documentClicked)
-        }
-
-        const documentTouchEnded = function (e: Event) {
-            if (documentTouchIsClick) {
-                documentClicked(e)
-            }
-
-            documentTouchIsClick = true
-        }
-
-        const documentClicked = function (e: Event) {
-            if (!(e.__clickedElements || []).includes(el)) {
-                return
-            }
-
-            console.log('clicked out')
+    const documentClick = useCallback((ev: Event) => {
+        if (!ev.__wrapperIsClicked) {
             onClickOut()
         }
-
-        const elementTouchMoved = function (e: Event) {
-            elTouchIsClick = false
-        }
-
-        const elementTouchEnded = function (e: Event) {
-            if (elTouchIsClick) {
-                elementClicked(e)
-            }
-
-            elTouchIsClick = true
-        }
-
-        const elementClicked = function (e: Event) {
-            e.__clickedElements = e.__clickedElements || []
-            e.__clickedElements.push(el)
-        }
-
-        return {
-            documentTouchMoved,
-            documentTouchStarted,
-            documentTouchEnded,
-            documentClicked,
-            elementTouchMoved,
-            elementTouchEnded,
-            elementClicked
-        }
-    }, [wrapperRef.current])
-
-    const toggleListeners = useCallback((add = true) => {
-        const el = wrapperRef.current as HTMLDivElement
-
-        const method = add ? 'addEventListener' : 'removeEventListener'
-
-        el?.[method]('touchmove', functions.elementTouchMoved as any)
-        el?.[method]('touchend', functions.elementTouchEnded as any)
-        el?.[method]('click', functions.elementClicked as any)
-
-        document[method]('touchmove', functions.documentTouchMoved as any)
-        document[method]('touchstart', functions.documentTouchStarted as any)
-        document[method]('touchend', functions.documentTouchEnded as any)
-        document[method]('click', functions.documentClicked as any)
-
-    }, [wrapperRef.current])
+    }, [onClickOut])
 
     useEffect(() => {
-        setTimeout(toggleListeners, 10)
-
+        const wrapper = wrapperRef.current
+        setTimeout(() => {
+            if (wrapper) {
+                wrapper.addEventListener('click', wrapperClick)
+                document.addEventListener('click', documentClick)
+            }
+        }, 10)
         return () => {
-            toggleListeners(false)
+            if (wrapper) {
+                wrapper.removeEventListener('click', wrapperClick)
+                document.removeEventListener('click', documentClick)
+            }
         }
-    }, [wrapperRef.current])
+    }, [wrapperRef.current, wrapperClick, documentClick])
 }
